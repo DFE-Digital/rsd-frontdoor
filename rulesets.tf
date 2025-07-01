73e7c4791ff0c262d78b50356ab9d132fc2bd611
Ash Davies <ash.davies@education.gov.uk>
2025-07-01 17:28:18 +0100

    Always use the target backend origin's forwarding protocol
    * This avoids a mismatch if the origin only supports Http but the rule route override is set to Matchrequest

--- rulesets.tf
+++ rulesets.tf
@@ -210,7 +210,7 @@ resource "azurerm_cdn_frontdoor_rule" "complete_dotnet_ruby_migration" {
   actions {
     route_configuration_override_action {
       cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.rsd["complete-dotnet"].id
-      forwarding_protocol           = "MatchRequest"
+      forwarding_protocol           = azurerm_cdn_frontdoor_route.rsd["complete-dotnet"].forwarding_protocol
       cache_behavior                = "Disabled"
     }
 
