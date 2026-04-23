c262a4bbc9f015e870b62ce70f310b11907280f5
Edward Balm <edwardbalm@gmail.com>
2026-04-23 11:34:37 +0100

    Implement checkov fixes.

--- tfvars-storage.tf
+++ tfvars-storage.tf
@@ -1,29 +1,31 @@
 resource "azurerm_storage_account" "tfvars" {
-  #checkov:skip=CKV_AZURE_59: Suppressing check pending review.
-  #checkov:skip=CKV_AZURE_33: Suppressing check pending review.
-  #checkov:skip=CKV_AZURE_190: Suppressing check pending review.
-  #checkov:skip=CKV_AZURE_206: Suppressing check pending review.
-  #checkov:skip=CKV2_AZURE_41: Suppressing check pending review.
-  #checkov:skip=CKV2_AZURE_38: Suppressing check pending review.
-  #checkov:skip=CKV2_AZURE_40: Suppressing check pending review.
-  #checkov:skip=CKV2_AZURE_1: Suppressing check pending review.
-  #checkov:skip=CKV2_AZURE_47: Suppressing check pending review.
-  #checkov:skip=CKV2_AZURE_33: Suppressing check pending review.
+  #checkov:skip=CKV_AZURE_33: Ensure Storage logging is enabled for Queue service for read, write and delete requests
+  #checkov:skip=CKV_AZURE_206: Ensure that Storage Accounts use replication
+  #checkov:skip=CKV2_AZURE_1: Ensure storage for critical data are encrypted with Customer Managed Key
+  #checkov:skip=CKV2_AZURE_33: Ensure storage account is configured with private endpoint
 
-  name                          = "${local.environment}rsdfdfvars"
-  resource_group_name           = local.resource_group.name
-  location                      = local.azure_location
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
-  min_tls_version               = "TLS1_2"
-  https_traffic_only_enabled    = true
-  public_network_access_enabled = true
+  name                            = "${local.environment}rsdfdfvars"
+  resource_group_name             = local.resource_group.name
+  location                        = local.azure_location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
 
   tags = local.tags
 }
 
 resource "azurerm_storage_container" "tfvars" {
-  # checkov:skip=CKV2_AZURE_21: Suppressing check pending review.
+  # checkov:skip=CKV2_AZURE_21: Ensure Storage logging is enabled for Blob service for read requests
 
   name                  = "${local.environment}rsdfdfvars"
   storage_account_name  = azurerm_storage_account.tfvars.name
@@ -41,11 +43,9 @@ resource "azurerm_storage_blob" "tfvars" {
 }
 
 resource "azurerm_storage_account_network_rules" "tfvars" {
-  #checkov:skip=CKV_AZURE_35: Suppressing check pending review.
-
   storage_account_id         = azurerm_storage_account.tfvars.id
-  default_action             = "Allow"
-  bypass                     = []
+  default_action             = "Deny"
+  bypass                     = ["AzureServices"]
   virtual_network_subnet_ids = []
 }
 
